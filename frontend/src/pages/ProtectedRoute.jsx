e5332de06b09300a96bec6a38e6b8e22663d04b3
M.Jayachaithanyaram <jayachaithanyaram@gmail.com>
2026-05-20 12:19:18 +0530

    created frontend ProtectedRoute page

---
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+
+  const token = localStorage.getItem("token");
+
+  // If no token → redirect login
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  // If token exists → allow access
+  return children;
+}
+
+export default ProtectedRoute;
